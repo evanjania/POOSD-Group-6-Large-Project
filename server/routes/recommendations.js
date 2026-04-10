90b24293152d73b6c2e4eb0936eafa08554d7c9e
Logan <loganelkins0101@gmail.com>
2026-04-10 13:15:50 -0400

    Add edit recommendations api

--- server/routes/recommendations.js
+++ server/routes/recommendations.js
@@ -1,30 +1,13 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 const router = express.Router();
-
-// Database info
-const url = 'mongodb+srv://kevin:kevin123@largeprojectcluster.xezylnw.mongodb.net/largeProjectDB?appName=LargeProjectCluster';
-const client = new MongoClient(url);
-const dbName = 'largeProjectDB'
-
-const rec = {
-    userId: "",
-    title: "",
-    category: "",
-    description: "",
-    location: "",
-    rating: 0,
-    visibility: "",
-}
 
 // Add recommendation
 router.post('/add', async (req, res) => {
-    try
-    {
+    try{
         // Parse and validate data from request
         const{username, title, category, rating, notes} = req.body;
-        if(!username || !title || !category || !rating || !notes)
-        {
+        if(!username || !title || !category || !rating || !notes){
             return res.status(400).json({error: "Missing data"});
         }
 
@@ -35,8 +18,7 @@ router.post('/add', async (req, res) => {
         const existingRec = await recsCollection.findOne({username, title, category})
 
         // Check if existingRec title and categroy match
-        if(existingRec)
-        {
+        if(existingRec){
             return res.status(400).json({error: "This user already has a recommendation for this title and category already"})
         }
 
@@ -53,12 +35,82 @@ router.post('/add', async (req, res) => {
     }
 
     // Handle errors
-    catch(err)
-    {
+    catch(err){
         console.error("ERROR ADDING RECOMMENDATION: ", err);
         res.status(500).json({error: "Server error"});
     }
 });
 
+// Edit recommendation
+router.patch('/edit', async (req, res) => {
+    try{
+        // Parse and validate data from request
+        const{id,  
+              title, 
+              category, 
+              rating, 
+              notes} = req.body;
+
+        // Check id
+        if(!id){
+            return res.status(400).json({error: "Missing recommendation id"});
+        }
+
+        if(!ObjectId.isValid(id)){
+            return res.status(400).json({error: "Invalid recommendation id"});
+        }
+
+        // Valid optional fields
+        /*if(category !== undefined && 
+            (category === "movie"
+            || category === "tv"
+            || category === "music")
+        ){
+            return res.status(400).json({error: "Category must be movie, tv, or music"})
+        }*/
+
+        if(rating !== undefined && (rating < 1 || rating > 5)){
+            return res.status(400).json({error: "Rating must be 1-5"});
+        }
+  
+        // Create db connection
+        const db = req.db;
+        const recsCollection = db.collection('recommendations');
+
+        // Create objects to update recommendation
+        const query = {_id: new ObjectId(id)};
+
+        const updateFields = {};
+        if(title !== undefined)
+            updateFields.title = title;
+        if(category !== undefined)
+            updateFields.category = category;
+        if(rating !== undefined)
+            updateFields.rating = rating;
+        if(notes !== undefined)
+            updateFields.notes = notes;
+
+        if(Object.keys(updateFields).length === 0) {
+            return res.status(400).json({error: "No fields to update"});
+        }
+        
+        // Update recommendation
+        const result = await recsCollection.updateOne(
+            query, 
+            {$set: updateFields});
+
+        if(result.matchedCount === 0)
+            return res.status(404).json({error: "Recommendation not found"});
+
+        // Send success response
+        const updatedRec = await recsCollection.findOne(query);
+        res.status(200).json(updatedRec);
+    }
+    catch(err)
+    {
+        console.error("ERROR EDITING RECOMMENDATION: ", err);
+        res.status(500).json({error: "Server error"});
+    }
+});
 
 export default router;
